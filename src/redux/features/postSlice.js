da77cae94e86cad7cbd5baa75d9852ebca01a1af
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-03 11:27:22 +0300

    Finished writin the getPost async thunk

--- src/redux/features/postSlice.js
+++ src/redux/features/postSlice.js
@@ -1,1 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+
+export const getPost = createAsyncThunk("post/getPost", async ({ id }) => 
+{
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) => res.json())
+})
