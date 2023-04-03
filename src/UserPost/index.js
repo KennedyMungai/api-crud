a255f27cac9c53ea9d8d08d23341f8583f67a62c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-03 16:11:57 +0300

    The edit and delete buttons are only shown if the user is in edit mode

--- src/UserPost/index.js
+++ src/UserPost/index.js
@@ -110,7 +110,7 @@ const UserPost = () =>
                                         }
                                         <span>{post[0].body}</span>
                                     </Card>
-                                    <Space
+                                    {edit && (<Space
                                         size='middle'
                                         style={{ marginTop: 35, marginLeft: 5, float: 'right' }}
                                     >
@@ -130,7 +130,7 @@ const UserPost = () =>
                                         >
                                             Edit
                                         </Button>
-                                    </Space>
+                                    </Space>)}
                                 </div>)
                             }
                         </>
