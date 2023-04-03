a82c1653b258296c73ac67672d53d91ce44bf115
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-03 11:31:58 +0300

    Created postSlice

--- src/redux/features/postSlice.js
+++ src/redux/features/postSlice.js
@@ -5,3 +5,30 @@ export const getPost = createAsyncThunk("post/getPost", async ({ id }) =>
 {
     return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) => res.json())
 })
+
+const postSlice = createSlice(
+    {
+        name: "post",
+        initialState: {
+            post: [],
+            loading: false,
+            error: null
+        },
+        extraReducers: {
+            [getPost.pending]: (state, action) =>
+            {
+                state.loading = true
+            },
+            [getPost.fulfilled]: (state, action) =>
+            {
+                state.loading = false
+                state.post = [action.payload]
+            },
+            [getPost.rejected]: (state, action) =>
+            {
+                state.loading = false
+                state.error = action.payload
+            }
+        }
+    }
+)
