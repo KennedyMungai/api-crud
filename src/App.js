553d1622905f7f4df145b2a0e7ea5c78309e0f6e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-03 10:50:00 +0300

    Added the createPost route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import UserPost from './UserPost';
+import CreatePost from './UserPost/CreatePost';
 
 
 function App()
@@ -9,6 +10,7 @@ function App()
     <div className="App">
       <Routes>
         <Route path="/" element={<UserPost />} />
+        <Route path='/createPost' element={<CreatePost />} />
       </Routes>
     </div>
   );
