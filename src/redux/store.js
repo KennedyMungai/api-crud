e705153c1ea12c9ddb502d1a581385cb8bc50a94
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-03 11:35:24 +0300

    Configured the Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "./features/postSlice";
+
+
+export default configureStore(
+    {
+        reducer: {
+            app: postReducer
+        }
+    }
+)
