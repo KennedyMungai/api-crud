05a6216fb0f51e671226bbeac7d10766fa748612
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-03 10:59:02 +0300

    Added an input element and styled it

--- src/UserPost/index.js
+++ src/UserPost/index.js
@@ -9,6 +9,13 @@ const UserPost = () =>
     return (
         <div className="container">
             <h1 style={{ textAlign: 'center' }}>Fetch Posts</h1>
+            <Input
+                placeholder='Enter User Id'
+                type='number'
+                onChange={(e) => setId(e.target.value)}
+                value={id}
+                style={{ width: '300px' }}
+            />
         </div>
     )
 }
