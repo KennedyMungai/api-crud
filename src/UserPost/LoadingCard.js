34b6087da398ddaf1b9ae8da514437fbe7079010
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-03 12:02:35 +0300

    Wrote the code to render out the cards

--- src/UserPost/LoadingCard.js
+++ src/UserPost/LoadingCard.js
@@ -2,10 +2,28 @@ import { Card, Skeleton } from 'antd'
 import React from 'react'
 
 
-const LoadingCard = () =>
+const LoadingCard = ({ count }) =>
 {
+    const cards = () =>
+    {
+        let totalCards = []
+
+        for (let i = 0; i < count; i++)
+        {
+            totalCards.push(
+                <Card className="col-md-12" key={i}>
+                    <Skeleton active></Skeleton>
+                </Card>
+            )
+        }
+
+        return totalCards
+    }
+
     return (
-        <div>LoadingCard</div>
+        <div className="row pb-5">
+            {cards()}
+        </div>
     )
 }
 
